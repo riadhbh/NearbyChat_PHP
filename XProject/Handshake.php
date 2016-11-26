c23bb1e49f2b7ace4d32269e4ee87e73092a100b
riadhbh <raffle@live.fr>
2016-11-26 17:56:14 +0100

    first commit

---
+++ XProject/Handshake.php
@@ -0,0 +1,6 @@
+<?php
+$msg=$_POST["msg"];
+if($msg=="Are you here ?")
+	echo "Yes i'm here";
+else echo "bye bye !";
+?>
---
+++ XProject/login.php
@@ -0,0 +1,20 @@
+ <?php  
+ require "conn.php";  
+ $user_mail =$_POST["login_mail"];  
+ $user_pass =$_POST["login_pass"]; 
+ $sql_query = "select id,firstname,lastname from users where email like '$user_mail' and password like '$user_pass';";  
+ $result = mysqli_query($conn,$sql_query);  
+ if(mysqli_num_rows($result) >0 )  
+ {  
+ $row = mysqli_fetch_assoc($result);
+ $id=$row["id"]; 
+ $fn =$row["firstname"];
+ $ln= $row["lastname"]; 
+ echo $id."  Welcome  ".$fn." ".$ln;  
+ }  
+ else  
+ {   
+ echo "User or password incorrect!";  
+ }
+mysqli_close($conn); 
+ ?>  
